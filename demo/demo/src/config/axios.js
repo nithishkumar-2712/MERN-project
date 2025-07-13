6f80a9a5aa0cdd5135e4e4d70ad29193c57cebcb
nithishkumar-2712 <nithishkumar27122002@gmail.com>
2025-07-14 01:18:57 +0530

    lasstcommite

--- demo/demo/src/config/axios.js
+++ demo/demo/src/config/axios.js
@@ -1,7 +1,7 @@
 
 import Axios from "axios";
 const axios = Axios.create({
-  baseURL: "https://nithishkumar-2712.github.io/:3000", // ✅ Backend server URL
+  baseURL: "https://localhost:3000", // ✅ Backend server URL
   withCredentials: true,            // ✅ Allow sending cookies (like JWT)
 });
 
