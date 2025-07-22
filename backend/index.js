2775876437a9ffd3a2cee3c0c1af6c8ada794710
nithishkumar-2712 <nithishkumar27122002@gmail.com>
2025-07-22 22:36:14 +0530

    changrddv

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ Connect()
 const port = process.env.PORT
 app.use(express.json())
 app.use("/public", express.static("public"));
-app.use(cors({origin:["http://localhost:5173"],credentials:true}))
+app.use(cors({origin:["https://forend.onrender.com"],credentials:true}))
 // app.use(cors({
 //   origin: ["http://localhost:5173", "https://maharajan-t-shirt.onrender.com"],
 //   credentials: true
